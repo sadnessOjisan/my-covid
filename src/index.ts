b9978ebd7598286275993947d0d2af8ce740f686
sadness_ojisan <sadness.ojisan@gmail.com>
2020-11-21 02:34:32 +0900

    show rect

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-alert("hello world!!");
+import Konva from "konva";
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const stage = new Konva.Stage({
+  container: "container",
+  width: width,
+  height: height,
+});
+
+const layer = new Konva.Layer();
+const rectX = stage.width() / 2 - 50;
+const rectY = stage.height() / 2 - 25;
+
+const box = new Konva.Rect({
+  x: rectX,
+  y: rectY,
+  width: 100,
+  height: 50,
+  fill: "#00D2FF",
+  stroke: "black",
+  strokeWidth: 4,
+  draggable: true,
+});
+
+// add cursor styling
+box.on("mouseover", function () {
+  document.body.style.cursor = "pointer";
+});
+box.on("mouseout", function () {
+  document.body.style.cursor = "default";
+});
+
+layer.add(box);
+stage.add(layer);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "./dist"),
+    filename: "build.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        use: [
+          {
+            loader: "ts-loader",
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+};
