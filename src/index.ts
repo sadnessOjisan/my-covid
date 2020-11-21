8755f25f0033d8476e2578a7c89ea37a26191fe7
sadnessOjisan <sadness.ojisan@gmail.com>
2020-11-21 18:59:19 +0900

    画像の単一選択とその回転・削除 (#4)
    * only select
    
    * pinpoint delete

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Konva from "konva";
 import Test from "./img/test.png";
 import A from "./img/a.png";
 import { Image as KonvaImage } from "konva/types/shapes/Image";
+import { Transformer } from "konva/types/shapes/Transformer";
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -12,39 +13,63 @@ const stage = new Konva.Stage({
   height: height,
 });
 
-const objs: KonvaImage[] = [];
+const images: KonvaImage[] = [];
+const selectedImages: Transformer[] = [];
 const layer = new Konva.Layer();
 
 function drawImage(imageObj: HTMLImageElement) {
-  const darthVaderImg: KonvaImage = new Konva.Image({
+  const id = Math.random().toString(32).substring(2);
+  const loadedImage: KonvaImage = new Konva.Image({
     image: imageObj,
     x: stage.width() / 2 - 200 / 2,
     y: stage.height() / 2 - 137 / 2,
     width: 200,
     height: 137,
     draggable: true,
+    name: id,
   });
 
   // add cursor styling
-  darthVaderImg.on("mouseover", function () {
+  loadedImage.on("mouseover", function () {
     document.body.style.cursor = "pointer";
   });
-  darthVaderImg.on("mouseout", function () {
+  loadedImage.on("mouseout", function () {
     document.body.style.cursor = "default";
   });
-
-  const tr1 = new Konva.Transformer({
-    nodes: [darthVaderImg],
+  loadedImage.on("click", () => {
+    const tr1 = new Konva.Transformer({
+      nodes: [loadedImage],
+      name: id,
+    });
+    // クリックされたものだけを選択状態にする
+    const oldImage = selectedImages.shift();
+    if (oldImage) {
+      oldImage.destroy();
+    }
+    selectedImages.push(tr1);
+    layer.draw();
+    layer.add(tr1);
   });
 
-  layer.add(darthVaderImg);
-  layer.add(tr1);
+  layer.add(loadedImage);
   stage.add(layer);
-  objs.push(darthVaderImg);
+  images.push(loadedImage);
 }
 
 window.del = function del(): void {
-  objs[0].remove();
+  const selectedImage = selectedImages[0];
+  if (!selectedImage) {
+    alert("画像を選択してください。");
+    return;
+  }
+  const name = selectedImage.name();
+  console.log("name", name);
+  selectedImage.destroy();
+  var shapes = stage.find(`.${name}`);
+  shapes.each((shape) => {
+    shape.destroy();
+  });
+  images.shift();
   layer.draw();
 };
 
